1be78e2421f36011741ca44160678a03a726d6fd
Michał Śledź <sledzson08@gmail.com>
2021-08-05 23:43:24 +0200

    Measure CPU load periodically

--- main.c
+++ main.c
@@ -1,49 +1,62 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
+#define INTERVAL 1
+#define ITERATIONS 10
+
 typedef int cpu_info_t[10];
+
 float get_cpu_load(cpu_info_t cpu_info);
+
+float cpu_loads[10];
 
 int main(int argc, char **argv) {
 
-  FILE *stat = fopen("/proc/stat", "r");
+  int ret;
   char cpu[10] = {0};
-  char cpu2[10] = {0};
-  cpu_info_t cpu_info = {0};
-  cpu_info_t cpu_info2 = {0};
-  int ret = fscanf(stat, "%s %d %d %d %d %d %d %d %d %d %d", cpu, &cpu_info[0],
-                   &cpu_info[1], &cpu_info[2], &cpu_info[3], &cpu_info[4],
-                   &cpu_info[5], &cpu_info[6], &cpu_info[7], &cpu_info[8],
-                   &cpu_info[9]);
+  cpu_info_t last_cpu_info = {0};
+  cpu_info_t current_cpu_info = {0};
+  FILE *stat;
 
-  printf("%s %d %d %d %d %d %d %d %d %d %d\n", cpu, cpu_info[0], cpu_info[1],
-         cpu_info[2], cpu_info[3], cpu_info[4], cpu_info[5], cpu_info[6],
-         cpu_info[7], cpu_info[8], cpu_info[9]);
-
-  fclose(stat);
-  sleep(1);
   stat = fopen("/proc/stat", "r");
-  ret = fscanf(stat, "%s %d %d %d %d %d %d %d %d %d %d", cpu2, &cpu_info2[0],
-               &cpu_info2[1], &cpu_info2[2], &cpu_info2[3], &cpu_info2[4],
-               &cpu_info2[5], &cpu_info2[6], &cpu_info2[7], &cpu_info2[8],
-               &cpu_info2[9]);
+  fscanf(stat, "%s %d %d %d %d %d %d %d %d %d %d", cpu, &last_cpu_info[0],
+         &last_cpu_info[1], &last_cpu_info[2], &last_cpu_info[3],
+         &last_cpu_info[4], &last_cpu_info[5], &last_cpu_info[6],
+         &last_cpu_info[7], &last_cpu_info[8], &last_cpu_info[9]);
+  fclose(stat);
+  for (int i = 0; i < ITERATIONS; i++) {
+    sleep(INTERVAL);
+    stat = fopen("/proc/stat", "r");
+    fscanf(stat, "%s %d %d %d %d %d %d %d %d %d %d", cpu, &current_cpu_info[0],
+           &current_cpu_info[1], &current_cpu_info[2], &current_cpu_info[3],
+           &current_cpu_info[4], &current_cpu_info[5], &current_cpu_info[6],
+           &current_cpu_info[7], &current_cpu_info[8], &current_cpu_info[9]);
 
-  printf("%s %d %d %d %d %d %d %d %d %d %d\n", cpu2, cpu_info2[0], cpu_info2[1],
-         cpu_info2[2], cpu_info2[3], cpu_info2[4], cpu_info2[5], cpu_info2[6],
-         cpu_info2[7], cpu_info2[8], cpu_info2[9]);
+    fclose(stat);
+    printf("%s %d %d %d %d %d %d %d %d %d %d\n", cpu, current_cpu_info[0],
+           current_cpu_info[1], current_cpu_info[2], current_cpu_info[3],
+           current_cpu_info[4], current_cpu_info[5], current_cpu_info[6],
+           current_cpu_info[7], current_cpu_info[8], current_cpu_info[9]);
 
-  long busy_time1 = cpu_info[0] + cpu_info[1] + cpu_info[2] + cpu_info[5] +
-                  cpu_info[6] + cpu_info[7];
-  long total_time1 = busy_time1 + cpu_info[3] + cpu_info[4];
+    long current_busy_time = current_cpu_info[0] + current_cpu_info[1] +
+                             current_cpu_info[2] + current_cpu_info[5] +
+                             current_cpu_info[6] + current_cpu_info[7];
+    long current_total_time =
+        current_busy_time + current_cpu_info[3] + current_cpu_info[4];
 
-  long busy_time2 = cpu_info2[0] + cpu_info2[1] + cpu_info2[2] + cpu_info2[5] +
-                  cpu_info2[6] + cpu_info2[7];
-  long total_time2 = busy_time2 + cpu_info2[3] + cpu_info2[4];
+    long last_busy_time = last_cpu_info[0] + last_cpu_info[1] +
+                          last_cpu_info[2] + last_cpu_info[5] +
+                          last_cpu_info[6] + last_cpu_info[7];
+    long last_total_time = last_busy_time + last_cpu_info[3] + last_cpu_info[4];
 
-  float totald = total_time2 - total_time1;
-  float busyd = busy_time2 - busy_time1;
-  printf("totald: %f, busyd: %f", totald, busyd);
-  float cpu_load = busyd / totald * 100.0f;
-  printf("CPU load: %f%%\n", cpu_load);
+    float totald = current_total_time - last_total_time;
+    float busyd = current_busy_time - last_busy_time;
+    printf("totald: %f, busyd: %f", totald, busyd);
+    float cpu_load = busyd / totald * 100.0f;
+    printf("CPU load: %f%%\n", cpu_load);
+    cpu_loads[i] = cpu_load;
+    memcpy(last_cpu_info, current_cpu_info, 10 * sizeof(float));
+  }
   return 0;
 }
